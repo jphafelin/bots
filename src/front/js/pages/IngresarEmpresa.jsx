e0bab1bb1b555b9a1e53e4d4b0fef2c5a6cc5e4c
jphafelin <josepablo.hafelin@gmail.com>
2023-04-18 14:02:30 +0000

    18/04/2023 16:02

--- src/front/js/pages/IngresarEmpresa.jsx
+++ src/front/js/pages/IngresarEmpresa.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/botones.css"
 import { useNavigate } from "react-router-dom"
@@ -10,6 +10,139 @@ import Papa from 'papaparse';
 export const IngresarEmpresa = () => {
   const { store, actions } = useContext(Context);
   const navigate = useNavigate();
+
+  const [value, setValue] = useState("");
+  const [value2, setValue2] = useState("");
+  const [value3, setValue3] = useState("");
+  const [value4, setValue4] = useState("");
+  const [value5, setValue5] = useState("");
+  const [value6, setValue6] = useState("");
+  const [value7, setValue7] = useState("");
+  const [value8, setValue8] = useState("");
+  const [value9, setValue9] = useState("");
+  const [value10, setValue10] = useState("");
+  const [value11, setValue11] = useState("");
+  const [value12, setValue12] = useState("");
+  const [value13, setValue13] = useState("");
+  const [value14, setValue14] = useState("");
+
+  function handleChange(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue(event.target.value.replace(";", ""));
+    } else {
+      setValue(event.target.value);
+    }
+  }
+  function handleChange2(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue2(event.target.value.replace(";", ""));
+    } else {
+      setValue2(event.target.value);
+    }
+  }
+  function handleChange3(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue3(event.target.value.replace(";", ""));
+    } else {
+      setValue3(event.target.value);
+    }
+  }
+  function handleChange4(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue4(event.target.value.replace(";", ""));
+    } else {
+      setValue4(event.target.value);
+    }
+  }
+  function handleChange5(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue5(event.target.value.replace(";", ""));
+    } else {
+      setValue5(event.target.value);
+    }
+  }
+  function handleChange6(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue6(event.target.value.replace(";", ""));
+    } else {
+      setValue6(event.target.value);
+    }
+  }
+  function handleChange7(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue7(event.target.value.replace(";", ""));
+    } else {
+      setValue7(event.target.value);
+    }
+  }
+  function handleChange8(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue8(event.target.value.replace(";", ""));
+    } else {
+      setValue8(event.target.value);
+    }
+  }
+  function handleChange9(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue9(event.target.value.replace(";", ""));
+    } else {
+      setValue9(event.target.value);
+    }
+  }
+  function handleChange10(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue10(event.target.value.replace(";", ""));
+    } else {
+      setValue10(event.target.value);
+    }
+  }
+  function handleChange11(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue11(event.target.value.replace(";", ""));
+    } else {
+      setValue11(event.target.value);
+    }
+  }
+  function handleChange12(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue12(event.target.value.replace(";", ""));
+    } else {
+      setValue12(event.target.value);
+    }
+  }
+  function handleChange13(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue13(event.target.value.replace(";", ""));
+    } else {
+      setValue13(event.target.value);
+    }
+  }
+  function handleChange14(event) {
+    if (event.target.value.indexOf(";") !== -1) {
+      alert("El carácter ';' no está permitido en este campo.");
+      setValue14(event.target.value.replace(";", ""));
+    } else {
+      setValue14(event.target.value);
+    }
+  }
+  function handleKeyPress(event) {
+    if (event.key === ";") {
+      event.preventDefault();
+    }
+  }
 
 
 
@@ -27,11 +160,15 @@ export const IngresarEmpresa = () => {
         <div className="row">
           <div className="text-start mx-3">
             <label className="label-id">ID:</label>
-            <input className="casilla-id col bg-light rounded" value="1234" disabled="disabled"></input>
+            <input className="casilla-id col bg-light rounded" maxlength="4" value="1234" disabled="disabled"></input>
             <label className="label-razon-social">RAZON SOCIAL:</label>
-            <input className=" casilla-razon-social col-6 text-uppercase rounded"></input>
+            <input className=" casilla-razon-social col-6 text-uppercase rounded" maxlength="45" value={value}
+      onChange={handleChange}
+      onKeyPress={handleKeyPress}></input>
             <label className="label-rut">RUT:</label>
-            <input className="casilla-rut col-2 text-uppercase rounded"></input>
+            <input className="casilla-rut col-2 text-uppercase rounded" maxlength="12" value={value2}
+      onChange={handleChange2}
+      onKeyPress={handleKeyPress}></input>
             <label className="label-estado">ESTADO:</label>
             <select className="col bg-primary text-light rounded">
               <option>VIGENTE</option>
@@ -45,9 +182,13 @@ export const IngresarEmpresa = () => {
           <div className="text-start mx-3">
             
             <label>NOMBRE FANTASIA:</label>
-            <input className="casilla-nombre-fantasia col-6 text-uppercase rounded" ></input>
+            <input className="casilla-nombre-fantasia col-6 text-uppercase rounded" maxlength="40" value={value3}
+      onChange={handleChange3}
+      onKeyPress={handleKeyPress}></input>
             <label className="label-giro">GIRO:</label>
-            <input className="casilla-giro col-4 text-uppercase rounded"></input>
+            <input className="casilla-giro col-4 text-uppercase rounded" maxlength="40" value={value4}
+      onChange={handleChange4}
+      onKeyPress={handleKeyPress}></input>
           </div>
 
         </div>
@@ -58,7 +199,9 @@ export const IngresarEmpresa = () => {
         <div className="row my-1">
           <div className="text-start py-1 mx-3">
             <label>DIRECCION:</label>
-            <input className="casilla-direccion col-4 text-uppercase rounded"></input>
+            <input className="casilla-direccion col-4 text-uppercase rounded" maxlength="40" value={value5}
+      onChange={handleChange5}
+      onKeyPress={handleKeyPress}></input>
             <label className="label-region">REGION:</label>
             <select className="col bg-primary text-light rounded">
               <option>01</option>
@@ -79,7 +222,9 @@ export const IngresarEmpresa = () => {
 
             </select>
             <label className="label-comuna">COMUNA:</label>
-            <input className="casilla-comuna col-4 text-uppercase rounded"></input>
+            <input className="casilla-comuna col-4 text-uppercase rounded" maxlength="20" value={value6}
+      onChange={handleChange6}  
+      onKeyPress={handleKeyPress}></input>
           </div>
         </div>
         
@@ -90,18 +235,26 @@ export const IngresarEmpresa = () => {
         <div className="row my-2">
           <div className="text-start mx-3">
             <label>NOMBRE:</label>
-            <input className="casilla-nombre col-4 text-uppercase rounded"></input>
+            <input className="casilla-nombre col-4 text-uppercase rounded" maxlength="35" value={value7}
+      onChange={handleChange7}
+      onKeyPress={handleKeyPress}></input>
             <label className="label-telefono">TELEFONO:</label>
-            <input className="casilla-telefono col-4 text-uppercase rounded"></input>
+            <input className="casilla-telefono col-4 text-uppercase rounded" maxlength="25" value={value8}
+      onChange={handleChange8}
+      onKeyPress={handleKeyPress}></input>
           </div>
 
         </div>
         <div className="row my-2">
           <div className="text-start mx-3">
             <label>EMAIL:</label>
-            <input className="casilla-email col-4 text-uppercase rounded"></input>
+            <input className="casilla-email col-4 text-uppercase rounded" maxlength="42" value={value9}
+      onChange={handleChange9}
+      onKeyPress={handleKeyPress}></input>
             <label className="label-cargo">CARGO:</label>
-            <input className="casilla-cargo col-4 text-uppercase rounded"></input>
+            <input className="casilla-cargo col-4 text-uppercase rounded" maxlength="23" value={value10}
+      onChange={handleChange10}
+      onKeyPress={handleKeyPress}></input>
           </div>
 
         </div>
@@ -111,18 +264,26 @@ export const IngresarEmpresa = () => {
         <div className="row my-2">
           <div className="text-start mx-3">
             <label>NOMBRE:</label>
-            <input className="casilla-nombre col-4 text-uppercase rounded"></input>
+            <input className="casilla-nombre col-4 text-uppercase rounded" maxlength="35" value={value11}
+      onChange={handleChange11}
+      onKeyPress={handleKeyPress}></input>
             <label className="label-telefono">TELEFONO:</label>
-            <input className="casilla-telefono col-4 text-uppercase rounded"></input>
+            <input className="casilla-telefono col-4 text-uppercase rounded" maxlength="25" value={value12}
+      onChange={handleChange12}
+      onKeyPress={handleKeyPress}></input>
           </div>
 
         </div>
         <div className="row my-2">
           <div className="text-start mx-3">
             <label>EMAIL:</label>
-            <input className="casilla-email col-4 text-uppercase rounded"></input>
+            <input className="casilla-email col-4 text-uppercase rounded" maxlength="42" value={value13}
+      onChange={handleChange13}
+      onKeyPress={handleKeyPress}></input>
             <label className="label-cargo">CARGO:</label>
-            <input className="casilla-cargo col-4 text-uppercase rounded"></input>
+            <input className="casilla-cargo col-4 text-uppercase rounded" maxlength="23" value={value14}
+      onChange={handleChange14}
+      onKeyPress={handleKeyPress}></input>
           </div>
 
         </div>
