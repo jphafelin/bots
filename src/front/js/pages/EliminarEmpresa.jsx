d9054961572094ae8233b7f0e05ace05131fc5a1
jphafelin <josepablo.hafelin@gmail.com>
2023-04-19 15:52:07 +0000

    19/04/2023 17:51

--- src/front/js/pages/EliminarEmpresa.jsx
+++ src/front/js/pages/EliminarEmpresa.jsx
@@ -2,9 +2,6 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/botones.css"
 import { useNavigate } from "react-router-dom"
-import { Link } from "react-router-dom";
-import logo from "../../img/LogoNewOffice.jpeg";
-import "../../styles/navbar.css"
 import rigoImageUrl from "../../img/Logo.jpg";
 import Papa from 'papaparse';
 
@@ -13,39 +10,12 @@ import Papa from 'papaparse';
 export const EliminarEmpresa = () => {
   const { store, actions } = useContext(Context);
   const navigate = useNavigate();
-
+  const myArray = store.tipo_evento;
 
   return (
     <div className="containter row justify-content-center dv-100">
-       <nav className="navbar p-1">
-			<div className="container-fluid row">
-				<div className="col-2">
-					<Link to="/menu">
-						<img src={logo} height="60px"></img>
-					</Link>
-				</div>
-				<div className="col-8 text-center justify-content-start ">
-					<h3>INGRESAR EMPRESA</h3>
-				</div>
-				<div className="col-2 text-end">
-					<p>X04-I1</p>
-					<div>
-					<button id="cerrar-sesion" className="text-light btn border border-3 border-dark">CERRAR SESION</button>
-					<button id="ayuda"className="mx-2 btn border border-3 border-dark">?</button>
-					</div>
-				</div>
-			</div>
 
-
-		</nav>
-
-      <div id="eliminar" className="row justify-content-center text-light border border-dark border-2 border-top-0">E L I M I N A R</div>
-
-      <div>
-
-        <button id="btn-volver" className="col-1 m-4 justify border border-3 border-dark text-light" onClick={volver => navigate("/empresa")}>VOLVER</button>
-
-      </div>
+      <div id="eliminar" className="row justify-content-center text-light border border-dark border-2 border-top-0">E L I M I N A R</div> <div> <button id="btn-volver" className="col-1 m-4 justify border border-3 border-dark text-light" onClick={volver => navigate("/empresa")}>VOLVER</button> </div>
 
       <div className="col-9 text-center border border-3 border-dark border-top-0 bg-light">
 
@@ -53,37 +23,37 @@ export const EliminarEmpresa = () => {
           <b>ELEGIR EMPRESA</b>
         </div>
 
-        
+
         <div className="row">
-         <div className="col-2 border border-dark"><b>RUT</b></div>
-         <div className="col-8 border border-dark"><b>RAZON SOCIAL</b></div>
-         <div className="col-2 border border-dark"><b>ESTADO</b></div>
+          <div className="col-2 border border-dark"><b>RUT</b></div>
+          <div className="col-8 border border-dark"><b>RAZON SOCIAL</b></div>
+          <div className="col-2 border border-dark"><b>ESTADO</b></div>
         </div>
 
 
         <div className="row">
-         <input className="col-2 text-center"></input>
-         <input className="col-8"></input>
-         <input className="col-2 text-center"></input>
+          <input className="col-2 text-center"></input>
+          <input className="col-8"></input>
+          <input className="col-2 text-center"></input>
         </div>
 
 
         <div className="eleccion">
+          {myArray.length === 0 ? (
+            <h1><span className="spam_no">No element in Array</span></h1>
+          ) : (
+            myArray.map((item, key = item.id) => (
 
-        <div className="row" onClick={EliminarEmpresaActual => navigate("/eliminar_empresa/1")}>
-         <div className="col-2 border border-dark"><b>99.999.999-A</b></div>
-         <div className="col-8 border border-dark text-start"><b>MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM</b></div>
-         <div className="col-2 border border-dark"><b>NO VIGENTE</b></div>
-        </div>
+              <div className="row" onClick={EliminarEmpresaActual => navigate("/consultar_empresa/1")}>
+                <div className="col-2 border border-dark"><b>{item.rut}</b></div>
+                <div className="col-8 border border-dark text-start"><b>{item.razon_social}</b></div>
+                <div className="col-2 border border-dark"><b>{item.estado}</b></div>
+              </div>
 
-        
 
-        <div className="row" onClick={EliminarEmpresaActual => navigate("/eliminar_empresa/1")}>
-         <div className="col-2 border border-dark"><b>99.999.999-A</b></div>
-         <div className="col-8 border border-dark text-start"><b>MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM</b></div>
-         <div className="col-2 border border-dark"><b>NO VIGENTE</b></div>
-        </div>
 
+            ))
+          )}
         </div>
       </div>
 
@@ -91,7 +61,7 @@ export const EliminarEmpresa = () => {
       <div className="row justify-content-center m-3">
 
       </div>
-      
+
 
 
 
